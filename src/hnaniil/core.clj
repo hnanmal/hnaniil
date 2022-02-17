1453a5d57736fdc0757072699be2dc400cab7442
pjmk007 <pjmk007@gmail.com>
2022-02-18 00:50:05 +0900

    adfasf

--- src/hnaniil/core.clj
+++ src/hnaniil/core.clj
@@ -13,9 +13,10 @@
   
   
   (println (tripple 51))
+
   )
 (def x 1)
-
+(+ x 2)
 (cond (= x 0) "영이다."
       (= x 1) "일이다."
       :else "다른거다.")
@@ -26,12 +27,12 @@
     (= rnd guess) (println "You got my guess right!")
     :else (println "Sorry... guess again!")))
 
-(defn test-x [x]
-  (cond
-    (< x 10) "less than"
-    (> x 20) "greater than"
-    :else "between"))
-(test-x 15)
+;; (defn test-x [x]
+;;   (cond
+;;     (< x 10) "less than"
+;;     (> x 20) "greater than"
+;;     :else "between"))
+;; (test-x 15)
 ;; (require '[clojure.tools.logging :as log])
 
 ;; (log/info "Hello World")
